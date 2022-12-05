f6c32e8c2d006ba240c8d437bd42b45a2769d9bf
Jared Williams <Jared.Williams101697@gmail.com>
2022-12-05 15:06:50 -0500

    Finish d04 - pt1

--- d04/pt1/dist/index.js
+++ d04/pt1/dist/index.js
@@ -25,6 +25,30 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
 const inputFile = process.argv[2];
-const rawData = fs.readFileSync(inputFile || 'inputTest.txt', 'utf8');
-const data = rawData.split('\n');
+const rawData = fs.readFileSync('input.txt', 'utf8');
+const data = rawData.split('\r\n');
+console.log(data[0].split(','));
+var totalValue = 0;
+data.forEach(item => {
+    var splitItem = item.split(',');
+    console.log(splitItem);
+    var itemOne = splitItem[0].split('-');
+    var itemTwo = splitItem[1].split('-');
+    console.log(itemOne);
+    console.log('Comparing: ' + parseInt(itemOne[0]) + ' > ' + parseInt(itemTwo[0]) + ' and ' + parseInt(itemOne[1]) + ' > ' + parseInt(itemTwo[1]));
+    if (parseInt(itemOne[0]) <= parseInt(itemTwo[0]) && parseInt(itemOne[1]) >= parseInt(itemTwo[1])) {
+        console.log('passed');
+        totalValue += 1;
+        console.log(totalValue);
+    }
+    else if (parseInt(itemOne[0]) >= parseInt(itemTwo[0]) && parseInt(itemOne[1]) <= parseInt(itemTwo[1])) {
+        console.log('passed inversely');
+        totalValue += 1;
+        console.log(totalValue);
+    }
+    else {
+        console.log('failed');
+    }
+});
+console.log(totalValue);
 //# sourceMappingURL=index.js.map
