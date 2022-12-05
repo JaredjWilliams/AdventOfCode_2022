1c0f7c1f9057f59f0e4751cf7d4bb9014e6c5142
Jared Williams <Jared.Williams101697@gmail.com>
2022-12-05 15:49:08 -0500

    finished day 4

--- d04/pt2/index.ts
+++ d04/pt2/index.ts
@@ -13,17 +13,25 @@ data.forEach(item => {
    var itemOne = splitItem[0].split('-')
 
    var itemTwo = splitItem[1].split('-')
-   console.log('Comparing: ' + parseInt(itemOne[0]) + ' > ' + parseInt(itemTwo[0]) + ' and ' + parseInt(itemOne[1]) + ' > ' + parseInt(itemTwo[1]))
-   if (parseInt(itemOne[0]) <= parseInt(itemTwo[0]) && parseInt(itemOne[1]) >= parseInt(itemTwo[1])) {
-    console.log('passed')
+   console.log('Comparing: ' + parseInt(itemOne[0]) + ' & ' + parseInt(itemTwo[1]) + ' and ' + parseInt(itemOne[1]) + ' & ' + parseInt(itemTwo[0]))
+   if (parseInt(itemOne[0]) >= parseInt(itemTwo[0]) && parseInt(itemOne[0]) <= parseInt(itemTwo[1])) {
+    console.log('passed if statement 1')
     totalValue += 1
     console.log(totalValue)
-   } else if (parseInt(itemOne[0]) >= parseInt(itemTwo[0]) && parseInt(itemOne[1]) <= parseInt(itemTwo[1])) {
-    console.log('passed inversely')
+   } else if (parseInt(itemOne[1]) >= parseInt(itemTwo[0]) && parseInt(itemOne[1]) <= parseInt(itemTwo[1])) {
+    console.log('passed if statement 2')
     totalValue += 1
     console.log(totalValue)
+   } else if (parseInt(itemTwo[0]) >= parseInt(itemOne[0]) && parseInt(itemTwo[0]) <= parseInt(itemOne[1])) {
+      console.log('passed if statement 3')
+      totalValue += 1
+      console.log(totalValue)
+   } else if (parseInt(itemTwo[1]) >= parseInt(itemOne[0]) && parseInt(itemTwo[1]) <= parseInt(itemOne[1])) {
+      console.log('passed if statement 4')
+      totalValue += 1
+      console.log(totalValue)
    } else {
-    console.log('failed')
+      console.log('failed')
    }
 })
 
