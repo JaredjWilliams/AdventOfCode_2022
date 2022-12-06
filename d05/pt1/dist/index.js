1e477e7b6aa3499ba57f705d735f2c08ec0f9d91
Jared Williams <Jared.Williams101697@gmail.com>
2022-12-06 13:26:56 -0500

    Finish d05 - pt1

--- d05/pt1/dist/index.js
+++ d05/pt1/dist/index.js
@@ -25,6 +25,45 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
 const inputFile = process.argv[2];
-const rawData = fs.readFileSync(inputFile || 'inputTest.txt', 'utf8');
-const data = rawData.split('\n');
+const rawData = fs.readFileSync('input.txt', 'utf8');
+const data = rawData.split('\r\n');
+console.log(data.length);
+var chart = data.splice(0, 9);
+var newChart = [];
+var directions = data.splice(1, data.length);
+var newDirections = [];
+directions.forEach(dir => {
+    newDirections.push(dir.replace('move', '|').replace('from', '|').replace('to', '|').replaceAll(' ', '').split('|'));
+});
+chart.forEach(cha => {
+    newChart.push(cha.split('|'));
+});
+console.log(newDirections);
+// data.replace('move', '|').replace('from', '|').replace('to', '|').replaceAll(' ', '').split('|')
+// for(let i = 0; i < data.length; i++) {
+//     var newData = data[i].replace('move', '|').replace('from', '|').replace('to', '|').replaceAll(' ', '').split('|')
+//     console.log(newData[0])
+//     console.log(newData[1])
+//     console.log(newData[2])
+// }
+for (let i = 0; i < newDirections.length; i++) {
+    var length = parseInt(newDirections[i][1]);
+    console.log('length: ' + length);
+    var from = parseInt(newDirections[i][2]) - 1;
+    console.log('from: ' + from);
+    var to = parseInt(newDirections[i][3]) - 1;
+    console.log('to: ' + to);
+    for (let j = 0; j < length; j++) {
+        var element = newChart[from].pop();
+        console.log('Current element: ' + element);
+        newChart[to].push(element);
+    }
+}
+console.log(newChart);
+// var str: string = "Infinitbility";
+// let char: string = str.substring(str.length - 1);
+// console.log(char)
+// console.log(str)
+// // Output
+// // 'y'
 //# sourceMappingURL=index.js.map
